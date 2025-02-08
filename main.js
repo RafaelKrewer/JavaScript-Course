419495e84031000e739fef8a8ba66dfff8dd4fe9
RafaelKrewer <cesar.krewer@gmail.com>
2025-02-08 18:09:03 -0300

    Implement game logic for rock-paper-scissors with alerts for results

--- main.js
+++ main.js
@@ -1,36 +1,73 @@
 /* || = ou
 ! = inverte valor booleano*/
 
-document.getElementById('rock').onclick = function () {
-  const randomNumber = Math.random();
-  if (randomNumber < 1/3) {
-    console.log('Rock');
-  } else if (randomNumber <= 2/3) {
-    console.log('Paper');
-  } else {
-    console.log('Scissors');
-  }
-};
+let computerMove = '';
+let result = '';
 
-document.getElementById('paper').onclick = 
-function () {
-  const randomNumber = Math.random();
-  if (randomNumber < 1/3) {
-    console.log('Rock');
-  } else if (randomNumber <= 2/3) {
-    console.log('Paper');
-  } else {
-    console.log('Scissors');
-  }
-};
+document.getElementById('rock').onclick =
+  function () {
+    const randomNumber = Math.random();
+    if (randomNumber < 1 / 3) {
+      computerMove = 'Rock';
+    } else if (randomNumber <= 2 / 3) {
+      computerMove = 'Paper';
+    } else {
+      computerMove = 'Scissors';
+    }
 
-document.getElementById('scissors').onclick = function () {
-  const randomNumber = Math.random();
-  if (randomNumber < 1/3) {
-    console.log('Rock');
-  } else if (randomNumber <= 2/3) {
-    console.log('Paper');
-  } else {
-    console.log('Scissors');
-  }
-};
+    if (computerMove === 'Rock') {
+      result = 'Draw';
+    } else if (computerMove === 'Paper') {
+      result = 'You lose';
+    } else {
+      result = 'You win';
+    }
+    alert(`You picked rock. The computer picked ${computerMove}. ${result}`);
+  };
+
+
+
+document.getElementById('paper').onclick =
+  function () {
+    const randomNumber = Math.random();
+    if (randomNumber < 1 / 3) {
+      computerMove = 'Rock';
+    } else if (randomNumber <= 2 / 3) {
+      computerMove = 'Paper';
+    } else {
+      computerMove = 'Scissors';
+    }
+
+    if (computerMove === 'Rock') {
+      result = 'You win';
+    } else if (computerMove === 'Paper') {
+      result = 'Draw';
+    } else {
+      result = 'You lose';
+    }
+    alert(`You picked paper. The computer picked ${computerMove}. ${result}`);
+  };
+
+
+
+document.getElementById('scissors').onclick =
+  function () {
+    const randomNumber = Math.random();
+    if (randomNumber < 1 / 3) {
+      computerMove = 'Rock';
+    } else if (randomNumber <= 2 / 3) {
+      computerMove = 'Paper';
+    } else {
+      computerMove = 'Scissors';
+    }
+
+    if (computerMove === 'Rock') {
+      result = 'You lose';
+    } else if (computerMove === 'Paper') {
+      result = 'You win';
+    } else {
+      result = 'Draw';
+    }
+    alert(`You picked scissors. The computer picked ${computerMove}. ${result}`);
+  };
+
