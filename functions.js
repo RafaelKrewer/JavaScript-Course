8f384c6101564f8e82c8159545a00262142aa490
RafaelKrewer <cesar.krewer@gmail.com>
2025-02-11 15:26:07 -0300

    commit

--- functions.js
+++ functions.js
@@ -1,68 +1,32 @@
-/* 
-function1 () {
-  console.log('Hello');
-}
-
-function1();
-*/
-
 let computerMove = '';
 let result = '';
 
-function pickComputerMove() {
+
+document.getElementById('rock').onclick = function () {
   const randomNumber = Math.random();
-  if (randomNumber < 1 / 3) {
-    computerMove = 'Rock';
-  } else if (randomNumber <= 2 / 3) {
-    computerMove = 'Paper';
-  } else {
-    computerMove = 'Scissors';
-  }
-}
 
-document.getElementById('rock').onclick =
+  computerMove = randomNumber < 1 / 3 ? 'Rock' : randomNumber <= 2 / 3 ? 'Paper' : 'Scissors';
+  result = computerMove === 'Rock' ? 'Draw' : computerMove === 'Paper' ? 'You lose' : 'You win';
 
-  function () {
-    pickComputerMove();
-    if (computerMove === 'Rock') {
-      result = 'Draw';
-    } else if (computerMove === 'Paper') {
-      result = 'You lose';
-    } else {
-      result = 'You win';
-    }
-    alert(`You picked rock. The computer picked ${computerMove}. ${result}`);
-  };
+  document.getElementById('message').textContent = `You picked rock. The computer picked ${computerMove}. ${result}`;
+};
 
 
+document.getElementById('paper').onclick = function () {
+  const randomNumber = Math.random();
 
-document.getElementById('paper').onclick =
-  function () {
-    pickComputerMove();
+  computerMove = randomNumber < 1 / 3 ? 'Rock' : randomNumber <= 2 / 3 ? 'Paper' : 'Scissors';
+  result = computerMove === 'Rock' ? 'You win' : computerMove === 'Paper' ? 'Draw' : 'You lose';
 
-    if (computerMove === 'Rock') {
-      result = 'You win';
-    } else if (computerMove === 'Paper') {
-      result = 'Draw';
-    } else {
-      result = 'You lose';
-    }
-    alert(`You picked paper. The computer picked ${computerMove}. ${result}`);
-  };
+  document.getElementById('message').textContent = `You picked paper. The computer picked ${computerMove}. ${result}`;
+};
 
 
+document.getElementById('scissors').onclick = function () {
+  const randomNumber = Math.random();
 
-document.getElementById('scissors').onclick =
-  function () {
-   pickComputerMove();
-   
-    if (computerMove === 'Rock') {
-      result = 'You lose';
-    } else if (computerMove === 'Paper') {
-      result = 'You win';
-    } else {
-      result = 'Draw';
-    }
-    alert(`You picked scissors. The computer picked ${computerMove}. ${result}`);
-  };
+  computerMove = randomNumber < 1 / 3 ? 'Rock' : randomNumber <= 2 / 3 ? 'Paper' : 'Scissors';
+  result = computerMove === 'Rock' ? 'You lose' : computerMove === 'Paper' ? 'You win' : 'Draw';
 
+  document.getElementById('message').textContent = `You picked scissors. The computer picked ${computerMove}. ${result}`;
+};
