3029823e2875fd60567cdfe557e06061c63d0a61
RafaelKrewer <cesar.krewer@gmail.com>
2025-02-12 15:49:26 -0300

    add initial HTML and JavaScript files for Rock Paper Scissors game

---
+++ objetos.js
@@ -0,0 +1,20 @@
+/*
+const produto = {
+    (propriedade)nome: 'meia',
+(propriedade também) preço: 10
+  };
+
+console.log(produto);
+console.log(produto.nome);
+console.log(produto.preço);
+
+produto.nome = 'meia cinza';
+console.log(produto); 
+
+    Agora com o novo valor, vai mostrar 'meia cinza'.
+
+  produto.newProperty = true; // Cria uma nova propriedade.
+
+delete produto.newProperty; // Deleta a propriedade.
+*/
+
