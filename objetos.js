93ea95a0ba52c29d7fd3389799d96620c08ead52
RafaelKrewer <cesar.krewer@gmail.com>
2025-02-12 16:08:43 -0300

    implement game logic for Rock Paper Scissors with user interaction

--- objetos.js
+++ objetos.js
@@ -18,3 +18,39 @@ console.log(produto);
 delete produto.newProperty; // Deleta a propriedade.
 */
 
+let computerMove = '';
+let result = '';
+
+
+document.getElementById('rock').onclick =
+  function () {
+    const randomNumber = Math.random();
+
+    computerMove = randomNumber < 1 / 3 ? 'Rock' : randomNumber <= 2 / 3 ? 'Paper' : 'Scissors';
+    result = computerMove === 'Rock' ? 'Draw' : computerMove === 'Paper' ? 'You lose' : 'You win';
+
+    document.getElementById('message').textContent = `You picked rock. The computer picked ${computerMove}. ${result}`;
+  };
+
+
+document.getElementById('paper').onclick =
+  function () {
+    const randomNumber = Math.random();
+
+    computerMove = randomNumber < 1 / 3 ? 'Rock' : randomNumber <= 2 / 3 ? 'Paper' : 'Scissors';
+    result = computerMove === 'Rock' ? 'You win' : computerMove === 'Paper' ? 'Draw' : 'You lose';
+
+    document.getElementById('message').textContent = `You picked paper. The computer picked ${computerMove}. ${result}`;
+  };
+
+
+document.getElementById('scissors').onclick =
+  function () {
+    const randomNumber = Math.random();
+
+    computerMove = randomNumber < 1 / 3 ? 'Rock' : randomNumber <= 2 / 3 ? 'Paper' : 'Scissors';
+    result = computerMove === 'Rock' ? 'You lose' : computerMove === 'Paper' ? 'You win' : 'Draw';
+
+    document.getElementById('message').textContent = `You picked scissors. The computer picked ${computerMove}. ${result}`;
+  };
+
