fbcd037efbdb52b72b19593ec4e6ed1ee68f0061
RafaelKrewer <cesar.krewer@gmail.com>
2025-02-13 17:42:58 -0300

    add exercises for functions and objects with example implementations

--- exercicios.js
+++ exercicios.js
@@ -1,3 +1,5 @@
+/*
+// Exercício 1: Funções
 let randomNumber = Math.random();
 let result = '';
 let guess = 'Heads';
@@ -6,3 +8,27 @@ console.log('Random number: ' + randomNumber.toFixed(1));
 randomNumber < 0.5 ? result = 'Heads' : result = 'Tails';
 
 guess === result ? console.log('You win! Computer said: ' + result) : console.log('You lose! Computer said: ' + result);
+
+*/
+
+// Exercício 2: Objetos
+
+
+const pessoa = {
+  nome: 'João',
+  altura: 1.80,
+  peso: 90,
+  idade: 20,
+}
+
+pessoa.hobbies = ['futebol', ' xadrez', ' programação,']; //array
+
+const pessoa2 = {
+  nome: 'Maria',
+  altura: 1.65,
+  peso: 57,
+  idade: 14,
+  hobbies: [' natação', ' leitura', ' música'], 
+}
+
+console.log(pessoa.hobbies + pessoa2.hobbies);
