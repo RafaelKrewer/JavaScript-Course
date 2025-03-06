63606c45fbb1073662c1de6cf6e22383890332f7
RafaelKrewer <rafaelkrewer5@gmail.com>
2025-03-06 19:40:07 -0300

    refactor: improve input handling and update shipping cost logic in Amazon calculator

--- 09-DOM/09-amazoncalc.js
+++ 09-DOM/09-amazoncalc.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', function () {
   function calculateTotal() {
     const inputElem = document.querySelector('.input'); // Seleciona o elemento de entrada
-    let cost = parseFloat(inputElem.value); // Converte o valor de entrada para um número de ponto flutuante
+    let cost = Number(inputElem.value); // Converte o valor de entrada para um número de ponto flutuante
 
     // Verifica se o valor de entrada é um número válido
     if (isNaN(cost)) {
@@ -9,10 +9,14 @@ document.addEventListener('DOMContentLoaded', function () {
       return; // Sai da função se o valor não for um número
     }
 
-    // Adiciona o custo de envio de $10 se o valor for menor que $40
-    if (cost < 40) {
+    if (cost <= 0) {
+      cost = cost;
+    } else if (cost > 0 || cost < 40) {
       cost += 10; // Adiciona o custo de envio de $10
     }
+    
+
+   
 
     // Atualiza a mensagem com o custo total
     document.querySelector('.message').innerHTML = `Total: $${cost.toFixed(2)}`;
@@ -20,6 +24,12 @@ document.addEventListener('DOMContentLoaded', function () {
 
   // Adiciona o evento de clique ao botão
   document.getElementById('calculateButton').addEventListener('click', calculateTotal);
+
+  document.querySelector('.input').addEventListener('keypress', function (e) {
+    if (e.key === 'Enter') {
+      calculateTotal();
+    }
+  });
 
   document.getElementById('resetValue').addEventListener('click', function () {
     document.querySelector('.input').value = ''; // Limpa o valor de entrada
